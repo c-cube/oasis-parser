199956df8ca70805c3d5b6d99ea99d24103f62a1
Simon Cruanes <simon.cruanes.2007@m4x.org>
2016-04-29 22:31:07 +0200

    oasis_check: print statements if `--print` is given

--- src/tools/oasis_check.ml
+++ src/tools/oasis_check.ml
@@ -3,29 +3,39 @@
 
 (** {1 Simple Tool to Check _oasis files} *)
 
+module A = Oasis_ast
 module P = Oasis_parser
 
-let check_file debug file =
-  if debug then Format.printf "check file `%s`@." file;
+type conf = {
+  debug: bool;
+  print: bool;
+}
+
+let check_file conf file =
+  if conf.debug then Format.printf "check file `%s`@." file;
   try
-    let _stmts = P.parse_file file in
-    if debug then Format.printf "success@.";
-    (* TODO: with another flag, print back *)
+    let stmts = P.parse_file file in
+    if conf.debug then Format.printf "success (%d statements)@." (List.length stmts);
+    if conf.print
+    then List.iter (Format.printf "%a@." A.pp_top_stmt) stmts;
   with e ->
     let stack = Printexc.get_backtrace() in
-    Format.printf "error: %s%s" (Printexc.to_string e) stack;
+    Format.printf "error: %s%s@." (Printexc.to_string e) stack;
     exit 1
 
 let () =
   let l = ref [] in
   let debug = ref false in
+  let print = ref false in
   let options =
     Arg.align
       [ "-d", Arg.Set debug, " enable debug mode"
+      ; "--print", Arg.Set print, " print parsed statements"
       ]
   in
   Arg.parse
     options
     (fun file -> l := file :: !l)
     "usage: oasis_check [options] file [,file]*";
-  List.iter (check_file !debug) (List.rev !l)
+  let conf = { debug= !debug; print= !print; } in
+  List.iter (check_file conf) (List.rev !l)
